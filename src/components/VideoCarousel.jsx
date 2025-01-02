008c00fc722cee01257012b40121c279cf52c66f
anaiva27 <nastia.iv.91@gmail.com>
2025-01-01 20:29:58 -0800

    mobile padding

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -129,7 +129,7 @@ const VideoCarousel = () => {
 					<div
 						key={list.id}
 						id="slider"
-						className="sm:pr-20 pr-30"
+						className="sm:pr-20 pr-10"
 					>
 						<div className="video-carousel_container">
 							<div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
