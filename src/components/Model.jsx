adb85cdefd0799d9c4c7a7f5d4e21039cfa7575c
anaiva27 <nastia.iv.91@gmail.com>
2025-01-18 22:02:00 -0800

    model start

---
+++ src/components/Model.jsx
@@ -0,0 +1,85 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useState } from "react";
+import { yellowImg } from "../utils";
+import { useRef } from "react";
+import * as THREE from "three";
+import { View } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
+const Model = () => {
+	const [size, setSize] = useState("small");
+	const [model, setModel] = useState({
+		title: "iPhone 15 Pro in Natural Titanium",
+		color: ["#8f8a81", "#ffe7b9", "#6f6c64"],
+		img: yellowImg,
+	});
+
+	const cameraControlSmall = useRef();
+	const cameraControlLarge = useRef();
+
+	const small = useRef(new THREE.Group());
+	const large = useRef(new THREE.Group());
+
+	const [smallRotation, setSmallRotation] = useState(0);
+	const [largeRotation, setLargeRotation] = useState(0);
+
+	useGSAP(() => {
+		gsap.to("#heading", {
+			y: 0,
+			opacity: 1,
+		});
+	}, []);
+
+	return (
+		<section className="common-padding">
+			<div className="screen-max-width">
+				<h1
+					id="heading"
+					className="section-heading"
+				>
+					Take c closer look.
+				</h1>
+				<div className="flex flex-col items-center mt-5">
+					<div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+						<ModelView
+							index={1}
+							groupRef={small}
+							gsapType="view1"
+							controlRef={cameraControlSmall}
+							setRotationState={setSmallRotation}
+							item={model}
+							size={size}
+						/>
+						<ModelView
+							index={2}
+							groupRef={large}
+							gsapType="view2"
+							controlRef={cameraControlLarge}
+							setRotationState={setLargeRotation}
+							item={model}
+							size={size}
+						/>
+						<Canvas
+							className="w-full h-full"
+							style={{
+								position: "fixed",
+								top: 0,
+								bottom: 0,
+								left: 0,
+								right: 0,
+								overflow: "hidden",
+							}}
+							eventSource={document.getElementById("root")}
+						>
+							<View.Port />
+						</Canvas>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Model;
